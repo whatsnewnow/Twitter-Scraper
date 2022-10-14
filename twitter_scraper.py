357a7d0b3d52856321a5543f922ebee264c1b7dd
shgnplaatjies <plaatjiesshagan@gmail.com>
2022-10-14 22:34:30 +0200

    Update twitter_scraper
    - Added 'api_keys.py' which is just renamed config.py, being picky.
    - added authentication to the twitter scaping script

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,8 +1,22 @@
 import pandas as pd
-import tweepy as tw
-import configparser 
+import tweepy
+import api_keys 
 
+# Authenticate to Twitter
+# This authentication approach isn't sustainable, temporarily solution for testing. 
+# Transfer to OAuth2 and configparser for production.
 
-config = configparser.RawConfigParser() # create config object important to preserve privacy after calling API
+def auth():
+    try: 
+        auth = tweepy.OAuthHandler(api_keys.CLIENT_KEY, api_keys.CLIENT_SECRET_KEY)
+        auth.set_access_token(api_keys.ACCESS_TOKEN, api_keys.ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
+    except:
+        print("Error: Authentication Failed")
+        
+def main():
+    auth()    
 
-
+# If this script file's name is main, run the main function.
+if __name__ == "__main__": 
+    main()
